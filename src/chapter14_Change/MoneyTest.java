90668d4185fcc94e2f02440338d6991a1d895c1c
gunji yasuhiro <gunji-yasuhiro@ksc.co.jp>
2020-03-10 09:24:05 +0900

    chapter14完了コード

--- src/chapter14_Change/MoneyTest.java
+++ src/chapter14_Change/MoneyTest.java
@@ -56,6 +56,11 @@ class MoneyTest {
 		Bank bank = new Bank();
 		bank.addRate("CHF", "USD", 2);
 		Money result = bank.reduce(Money.franc(3), "USD");
+		assertEquals(Money.dollar(1), result);
+	}
+	@Test
+	public void testIdentityRate() {
+		assertEquals(1,  new Bank().rate("USD", "USD"));
 	}
 }
 
